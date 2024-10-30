3c0cffceeb2a9263526b3d6deb8f28f713421a0c
retr0717 <alwinaji717@gmail.com>
2024-10-30 16:04:13 +0530

    profile header styling

--- app/(main)/profile.tsx
+++ app/(main)/profile.tsx
@@ -1,14 +1,105 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import { useAuth } from '@/contexts/AuthContext'
+import { useRouter } from 'expo-router'
+import Header from '@/components/Header'
+import { hp, wp } from '@/helpers/common'
+import Icon from '@/assets/icons'
+import { theme } from '@/constants/theme'
 
 const Profile = () => {
+
+  const {user, setAuth} = useAuth;
+  const router = useRouter();
+
+  const handleLogout = async() => {
+    //show confirm modal.
+  }
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <ScreenWrapper bg={"white"}>
+      <UserHeader user={user} router={router} handleLogout={handleLogout}/>
+    </ScreenWrapper>
   )
 }
 
+
+const UserHeader = ({user, router, handleLogout} : {user: any, router: any, handleLogout: any}) => {
+
+  return(
+    <View style={{flex: 1, backgroundColor: 'white', paddingHorizontal: wp(4)}}>
+      <View>
+        <Header title="Profile" showBackButton={true}/>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Icon name={'logout'} color={theme.colors.rose} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+} 
+
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    marginHorizontal: wp(4),
+    marginBottom: 20,
+  },
+  headerShape: {
+    width: wp(100),
+    height: hp(20),
+  },
+  avatarContainer: {
+    height: hp(12),
+    width: hp(12),
+    alignSelf: 'center',
+  },
+  editIcon: {
+    position: 'absolute',
+    bottom: 0,
+    right: -12,
+    padding: 7,
+    borderRadius: 50,
+    backgroundColor: 'white',
+    shadowColor: theme.colors.textLight,
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 5,
+    elevation: 7,
+  },
+  username: {
+    fontSize: hp(3),
+    fontWeight: '500',
+    color: theme.colors.textDark,
+  },
+  info: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  infoText: {
+    fontSize: hp(1.6),
+    fontWeight: '500',
+    color: theme.colors.textLight,
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: 'center',
+    color: theme.colors.text,
+  },
+  listStyle: {
+    paddingHorizontal: wp(4),
+    paddingBottom: 30,
+  },
+  logoutButton: {
+    position: 'absolute',
+    right: 0,
+    padding: 5,
+    borderRadius: theme.radius.sm,
+    backgroundColor: '#fee2e2',
+  },
+})
---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import BackButton from './BackButton'
+import { useRouter } from 'expo-router'
+import { theme } from '@/constants/theme'
+import { hp } from '@/helpers/common'
+
+const Header = ({title, showBackButton = false, mb=10, handleLogout}: {title:string, showBackButton: boolean, md: any, handleLogout: any}) => {
+
+    const router = useRouter();
+    return (
+    <View style={[styles.container, {marginBottom: mb}]}>
+      {
+        showBackButton && (
+            <View style={styles.showBackButton}>
+                <BackButton router={router}/>
+            </View>
+        )
+      }
+      <Text style={styles.title}>{title || ""}</Text>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 5,
+        gap: 10
+    },
+    showBackButton: {
+        position: 'absolute',
+        left: 0,
+    },
+    title: {
+        fontSize: hp(2.7),
+        fontWeight: theme.fonts.semibold,
+        color: theme.colors.textDark,
+    }
+})
