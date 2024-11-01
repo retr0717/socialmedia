2b2a617fe4bdfcfa2cc8ec4d80ed1d51214b39b1
retr0717 <alwinaji717@gmail.com>
2024-11-01 16:54:38 +0530

    new post image picker with remove feature

--- app/(main)/NewPost.tsx
+++ app/(main)/NewPost.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useRef, useState } from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import Header from '@/components/Header'
@@ -7,7 +7,12 @@ import { hp, wp } from '@/helpers/common'
 import Avatar from '@/components/Avatar'
 import { useAuth } from '@/contexts/AuthContext'
 import RichTextEditor from '@/components/RichTextEditor'
+import * as ImagePicker from 'expo-image-picker';
 import { useRouter } from 'expo-router'
+import Icon from '@/assets/icons'
+import Button from '@/components/Button'
+import { Image } from 'react-native'
+import { getSupabaseFileUrl } from '@/services/imageService'
 
 const NewPost = () => {
 
@@ -17,6 +22,69 @@ const NewPost = () => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(file);
+
+  const onPick = async (isImage:boolean) => {
+
+    let mediaConfig = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.7
+    }
+
+    if(!isImage){
+      mediaConfig = {
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: true
+      }
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+    if(!result.canceled)
+    {
+      setFile(result.assets[0]);
+    }
+
+  }
+
+  const isLocalFile = (file: any) => {
+    if(!file) return null;
+    if(typeof file == 'object') return true;
+
+    return false;
+  }
+
+  const getFileType = (file: any) => {
+    if(!file) return null;
+
+
+    if(isLocalFile(file)){
+      return file.type;
+    }
+
+    //console.log("get file : ",file);
+    //check the image or video for remote file.
+    if(file.includes('postImage'))
+    {
+      return 'image';
+    }
+
+    return 'video';
+  }
+
+  const getFileUrl = (file: any) => {
+    if(!file) return null;
+
+    if(isLocalFile(file)) return file.uri;
+
+    //return the remote url path.
+    return getSupabaseFileUrl(file)?.uri;
+  }
+
+  const onSubmt = async () => {
+    
+  }
 
   return (
     <ScreenWrapper bg={'white'}>
@@ -47,7 +115,45 @@ const NewPost = () => {
             />
           </View>
 
+          {
+            file && (
+              <View style={styles.file}>
+                {
+                  getFileType(file) == 'video' ? (
+                    <></>
+                  )
+                  :
+                  (
+                    <Image source={{uri : getFileUrl(file)}} resizeMode='cover' style={{flex : 1}} />
+                  )
+                }
+
+                <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+                  <Icon name='delete' size={20} color='white'/>
+                </Pressable>
+              </View>
+            )
+          }
+
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Add to your post</Text>
+            <View style={styles.mediaIcons}>
+              <TouchableOpacity onPress={() => onPick(true)}>
+                <Icon name='image' size={30} color={theme.colors.dark}/>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => onPick(false)}>
+                <Icon name='video' size={33} color={theme.colors.dark} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </ScrollView>
+        <Button
+          title='Post'
+          buttonStyle={{height: hp(6.2)}}
+          loading={loading}
+          hasShadow={false}
+          onPress={onSubmt}
+        />
       </View>
     </ScreenWrapper>
   )
@@ -61,6 +167,11 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     paddingHorizontal: wp(4),
     gap: 15,
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text
   },
   title: {
     //marginBottom: 10,
@@ -117,7 +228,7 @@ const styles = StyleSheet.create({
     //padding: 6
   },
   file: {
-    height: hp(30),
+    height: hp(40),
     width: '100%',
     borderRadius: theme.radius.xl,
     overflow: 'hidden',
@@ -130,6 +241,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 10,
     right: 10,
+    padding: 7,
+    borderRadius: 50,
+    backgroundColor: 'rgba(255, 0, 0, 0.6)'
     // shadowColor: theme.colors.textLight,
     // shadowOffset: {width: 0, height: 3},
     // shadowOpacity: 0.6,
